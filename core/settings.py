b975c9a04285f1de065383ebe91a4a3a8a709892
[Cuenta-oss] <[ejmenrique97@gmail.com]>
2022-10-13 14:47:05 -0500

    new correction databases settings

--- core/settings.py
+++ core/settings.py
@@ -79,12 +79,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        dj_database_url.config(
+    'default':dj_database_url.config(
             default='postgresql://postgres:postgres@localhost/postgres',
             conn_max_age=600
         )
-    }
 }
 
 
