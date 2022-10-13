afb99a428d9ae4d513ac55a086e6d7c0d5e2d6d8
[Cuenta-oss] <[ejmenrique97@gmail.com]>
2022-10-13 16:23:56 -0500

    errors corrections in build.sh

--- task/forms.py
+++ task/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import modelTask
-
 # This class is a form that is based on the modelTask model, and it has three fields: title,
 # description, and important.
 class taskForm(forms.ModelForm):
